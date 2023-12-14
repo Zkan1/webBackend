1e9079620e79dd727994b40edabc4eb3fea77e49
Özkan Kuruoğlu <92784850+Zkan1@users.noreply.github.com>
2023-12-14 18:58:09 +0300

    2.1. commit

--- app_api/models/venue.js
+++ app_api/models/venue.js
@@ -1,14 +1,5 @@
 var mongoose = require( "mongoose" );
 
-var venue = new mongoose.Schema({
-    name: {type:String,required:true},
-    adress: String,
-    rating: {type:Number,min:0,max:5,default:0},
-    coordinates:{type:Number,index:"2dsphere"},
-    foodanddrink: [String],
-    hours:[hour],
-    comments:[comment]
- });
 //hour ve comment için şema tanımlamaları
  var hour = new mongoose.Schema(
     {
@@ -27,3 +18,13 @@ var venue = new mongoose.Schema({
         date:{type:Date,default:Date.now}
     }
  );
+ 
+ var venue = new mongoose.Schema({
+    name: {type:String,required:true},
+    address: String,
+    rating: {type:Number,min:0,max:5,default:0},
+    coordinates:{type:Number,index:"2dsphere"},
+    foodanddrink: [String],
+    hours:[hour],
+    comments:[comment]
+ });
